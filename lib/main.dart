98d6e0716c20a970ef87fc0daf6d2d2f18ba2a98
BenaliSaidi <bsaidi127@gmail.com>
2020-12-24 23:28:43 +0100

    check the expiration date

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:mocha/home.dart';
-import 'package:mocha/addnewproduct.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
 import 'package:mocha/model/productList.dart';
-import 'package:mocha/displayProductList.dart';
-import 'package:mocha/CounterMenu.dart';
 import 'package:mocha/model/statList.dart';
+import 'package:mocha/ActiveAccount.dart';
+import 'package:mocha/model/endtime.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
   final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
   Hive.registerAdapter(NewListAdapter());
   Hive.registerAdapter(NewProductAdapter());
+  Hive.registerAdapter(PeriodAdapter());
 
   await Hive.openBox('product');
   await Hive.openBox('counterMorning');
   await Hive.openBox('counterEvening');
   await Hive.openBox('statMorning');
   await Hive.openBox('statEvening');
+  await Hive.openBox('period');
 
-  runApp(MaterialApp(
+// runApp(MaterialApp(home: Home(),));
 
-    initialRoute: '/home' ,
-    routes: {
-      '/home' : (context) => Home(),
-      '/addnewproduct' : (context) => AddProduct(),
-      '/displayProductList' : (context) => productList(),
-      '/CounterMenu' : (context) => CounterMenu(),
 
-    },
-  ),
-  );
+
+
+    var test = Hive.box('period').get('time').endtime;
+
+    if((DateTime.now().isAfter(test) == true)) {
+      runApp(MaterialApp(home: ActiveAccount(),));
+    }
+    else{
+      runApp(MaterialApp(home: Home(),));
+    }
+
+
+
+
+
+  // runApp(MaterialApp(
+  //
+  //   initialRoute: '/home' ,
+  //   routes: {
+  //     '/home' : (context) => Home(),
+  //     '/addnewproduct' : (context) => AddProduct(),
+  //     '/displayProductList' : (context) => productList(),
+  //     '/CounterMenu' : (context) => CounterMenu(),
+  //
+  //   },
+  // ),
+  // );
 }
 
 
